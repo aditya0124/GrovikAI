f84b6fc2194d5396c2677ccecea64b9400bbc836
aditya0124 <amanaditya763@gmail.com>
2025-08-26 23:50:08 +0530

    update auth.js & move error page into auth folder

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-        images: {
-    domains: ["avatars.githubusercontent.com", "randomuser.me","lh3.googleusercontent.com"], // ✅ allow GitHub avatars
+ 
+  images: {
+    domains: ["avatars.githubusercontent.com", "randomuser.me", "lh3.googleusercontent.com"],
   },
 };
 
