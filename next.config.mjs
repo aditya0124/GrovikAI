4b22321e83dee25e8e15a14c0385893391cb49ba
aditya0124 <139566319+aditya0124@users.noreply.github.com>
2025-08-27 10:39:45 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- 
+ eslint:{
+  ignoreDuringBuilds:true,
+ },//updatte this
   images: {
     domains: ["avatars.githubusercontent.com", "randomuser.me", "lh3.googleusercontent.com"],
   },
